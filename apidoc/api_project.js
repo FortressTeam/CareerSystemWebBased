8474fecbe5ac31fad145e89097e574e082646b33
Vien NT <thevien@outlook.com>
2016-03-31 03:03:26 +0700

    Update API

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-03-30T19:52:00.810Z",
+    "time": "2016-03-30T20:01:38.345Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
